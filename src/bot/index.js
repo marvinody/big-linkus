d4bf2cbc8208517bee4efbf764eb976239055d96
Manuel Bugallo <marvinody@gmail.com>
2020-06-05 22:36:41 -0400

    improves logging with some time based messages

--- src/bot/index.js
+++ src/bot/index.js
@@ -4,10 +4,11 @@ import qs from "qs";
 import dotenv from "dotenv";
 import moment from "moment";
 dotenv.config();
+
 import { getLatestTokens, refreshAccessToken, YoutubeApi } from "../utils";
 
-const postToDiscord = (ytBroadcast) => {
-  console.log("WE LIVE");
+const postToDiscord = (ytBroadcast, { username }) => {
+  console.timeLog("bot", `${username} (${ytBroadcast.id}) is live!`);
   const url = `https://www.youtube.com/watch?v=${ytBroadcast.id}`;
   const thumb = ytBroadcast.snippet.thumbnails.standard.url;
   return axios.post(process.env.DISCORD_WEBHOOK, {
@@ -26,18 +27,23 @@ const postToDiscord = (ytBroadcast) => {
 };
 
 (async () => {
+  console.time("bot", "fetching all users");
   // no nested query so we don't fetch all tokens
   const users = await User.findAll();
+  console.timeLog("bot", `${users.length} users found, checking each`);
   await Promise.all(
     users.map(async (user) => {
       try {
+        console.timeLog("bot", `checking ${user.username}`);
+        // some ID for this guy
         let [accessToken, refreshToken] = await getLatestTokens(user.id);
-
+        // do we need to refresh our ID?
         const expired = moment() > moment(accessToken.expires);
         if (expired) {
-          console.log(`${user.username} expired, fetching new`);
+          console.timeLog("bot", `${user.username} expired, fetching new`);
           accessToken = await refreshAccessToken(refreshToken.token, user.id);
         }
+        // now let's see their status
         const ytApi = YoutubeApi(accessToken.token);
         const ytBroadcast = await ytApi.getLatestBroadcast();
         const [broadcast, created] = await Broadcast.findOrCreate({
@@ -50,13 +56,14 @@ const postToDiscord = (ytBroadcast) => {
             userId: user.id,
           },
         });
+        // only two cases we care to show currently
         const changedToLive =
           broadcast.status !== "live" && ytBroadcast.status === "live";
         const newlyMadeLive = created && broadcast.status === "live";
 
         // if desirable state, let's post to discord
         if (changedToLive || newlyMadeLive) {
-          await postToDiscord(ytBroadcast);
+          await postToDiscord(ytBroadcast, user);
         }
 
         // if we haven't newly created, then let's just update it to w/e
@@ -66,13 +73,18 @@ const postToDiscord = (ytBroadcast) => {
           });
         }
       } catch (err) {
-        console.log(`Crashed on user: ${user.username} - ${user.id}`);
+        console.timeLog(
+          "bot",
+          `Crashed on user: ${user.username} - ${user.id}`
+        );
         if (err.isAxiosError) {
-          console.error(err.response.data.error);
+          console.timeLog("bot", err.response.data.error);
         } else {
-          console.error(err);
+          console.timeLog("bot", err);
         }
       }
     })
   );
+
+  console.timeEnd("bot", "done");
 })();
